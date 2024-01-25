be1f53c8c9e81ad8ad98dc2e36b371763b6f4d57
cplepage <lepage.charlesphilippe@gmail.com>
2024-01-24 23:43:38 -0500

    formatting

--- build.ts
+++ build.ts
@@ -2,37 +2,37 @@ import { build, Plugin } from "esbuild";
 import fs from "fs";
 import * as sass from "sass";
 
-if(fs.existsSync("dist"))
-    fs.rmSync("dist", { recursive: true });
+if (fs.existsSync("dist"))
+  fs.rmSync("dist", { recursive: true });
 
 
-const scssPlugin: Plugin  = {
-    name: 'scss',
-    setup(build) {
-      build.onLoad({ filter: /\.scss$/ }, (args) => ({
-        contents: sass.compile(args.path).css,
-        loader: 'css',
-      }))
-    },
-  }
+const scssPlugin: Plugin = {
+  name: 'scss',
+  setup(build) {
+    build.onLoad({ filter: /\.scss$/ }, (args) => ({
+      contents: sass.compile(args.path).css,
+      loader: 'css',
+    }))
+  },
+}
 
 await build({
-    entryPoints: ["src/webview/index.ts"],
-    outfile: "dist/webview/index.js",
-    loader: {
-        ".svg": "text"
-    },
-    plugins: [scssPlugin],
-    bundle: true,
-    format: "esm"
+  entryPoints: ["src/webview/index.ts"],
+  outfile: "dist/webview/index.js",
+  loader: {
+    ".svg": "text"
+  },
+  plugins: [scssPlugin],
+  bundle: true,
+  format: "esm"
 });
 
 await build({
-    entryPoints: ["src/api/index.ts"],
-    outfile: "dist/api/index.js",
-    bundle: true,
-    globalName: "api",
-    format: "iife"
+  entryPoints: ["src/api/index.ts"],
+  outfile: "dist/api/index.js",
+  bundle: true,
+  globalName: "api",
+  format: "iife"
 });
 
 fs.cpSync("src/webview/index.html", "dist/webview/index.html");
