b7c778a90686968039b1560758d958c173219005
cplepage <lepage.charlesphilippe@gmail.com>
2024-02-07 11:46:41 -0500

    files

---
+++ editor/api/projects/scan.ts
@@ -0,0 +1,13 @@
+import type { fs as globalFS} from "../../../src/api";
+
+declare var fs: typeof globalFS;
+
+const scanRecursive = (parent: string, item: { name: string, isDirectory: boolean }) => {
+    const itemPath = parent + "/" + item.name;
+    if (!item.isDirectory) return itemPath;
+    return scan(itemPath)
+}
+
+export const scan = (directory: string): string[] => {
+    return fs.readdir(directory).map(item => scanRecursive(directory, item)).flat()
+}
---
+++ platform/node/src/editorContext.ts
@@ -0,0 +1,30 @@
+import { Context } from "vm";
+import path from "path";
+import AdmZip from "adm-zip";
+import { buildWebview } from "./build";
+
+export default function(home: string, jsContext: Context) {
+    const resolvePath = (entrypoint: string) => path.join(home, entrypoint).split("\\").join("/");
+
+    jsContext.jsDirectory = path.resolve(process.cwd(), "..", "..", "src", "js");
+    jsContext.resolvePath = resolvePath;
+    jsContext.buildWebview = (entryPoint: string, outdir: string) => buildWebview(resolvePath(entryPoint), resolvePath(outdir));
+    jsContext.zip = (projectdir: string, items: string[], to: string) => {
+        var zip = new AdmZip();
+        items.forEach(item => {
+            const itemPathComponents = item.split("/");
+            const itemName = itemPathComponents.pop();
+            const itemDirectory = itemPathComponents.join("/");
+            const realpath = resolvePath(projectdir + "/" + item);
+            zip.addLocalFile(realpath, itemDirectory, itemName);
+        });
+
+        const out = resolvePath(to);
+        zip.writeZip(out);
+        open(out.split("/").slice(0, -1).join("/"));
+    }
+    jsContext.unzip = (to: string, zipData: number[] | Uint8Array) => {
+        const zip = new AdmZip(Buffer.from(zipData))
+        zip.extractAllTo(resolvePath(to));
+    }
+}
