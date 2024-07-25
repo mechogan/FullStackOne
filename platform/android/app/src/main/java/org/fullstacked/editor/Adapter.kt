0259a2b6b5ac4ed4fc24078f6c5e51c159a1cad9
cplepage <lepage.charlesphilippe@gmail.com>
2024-07-24 23:38:28 -0400

    demo unzip and inits git

--- platform/android/app/src/main/java/org/fullstacked/editor/Adapter.kt
+++ platform/android/app/src/main/java/org/fullstacked/editor/Adapter.kt
@@ -1,134 +1,257 @@
 package org.fullstacked.editor
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
+import org.json.JSONObject
+import java.io.ByteArrayInputStream
 import java.io.File
+import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.attribute.BasicFileAttributes
+
 
 open class Adapter(val projectId: String, baseDirectory: String) {
     val platform = "android"
     val fs: AdapterFS = AdapterFS(baseDirectory)
 
-    fun callAdapterMethod(methodPath: ArrayList<String>, body: String?, done: (maybeData: Any?) -> Void): Void? {
-        if(methodPath.isEmpty()) return done(null)
+    open fun getFile(path: String) : InputStream? {
+        val data = this.fs.readFile(path, false);
+        if(data is AdapterError) return null
+        return ByteArrayInputStream(data as ByteArray)
+    }
+
+    fun writeFile(path: String, data: ByteArray?, recursive: Boolean) : Any? {
+        if(recursive) {
+            val dir = path.split("/").dropLast(1)
+            this.fs.mkdir(dir.joinToString("/"))
+        }
+
+        return this.fs.writeFile(path, data ?: byteArrayOf())
+    }
+
+    open fun callAdapterMethod(methodPath: ArrayList<String>, body: String?): Any? {
+        if(methodPath.isEmpty()) return null
 
         val json = if(!body.isNullOrEmpty()) JSONArray(body) else JSONArray("[]")
 
-        val writeFile : (String, ByteArray?, Boolean) -> Any? = { path, data, recursive ->
-            if(recursive) {
-                val dir = path.split("/").dropLast(1)
-                this.fs.mkdir(dir.joinToString("/"))
-            }
-            if(data != null) {
-                this.fs.writeFile(path, data)
-            }
-        }
-
         when (methodPath.first()) {
-            "platform" -> return done(this.platform)
-            "fs" -> {
-                when (methodPath[1]) {
-                    "readFile" -> {
-                        var utf8 = false
-                        if(json.length() > 1) {
-                            val opt = json.getJSONObject(1)
-                            try {
-                                utf8 = opt.getString("encoding") == "utf8"
-                            } catch (_: Exception) { }
-                        }
-                        return done(this.fs.readFile(json.getString(0), utf8))
-                    }
-                    "writeFile" -> {
-                        var data: ByteArray? = null
-
-                        try {
-                            if(json.getJSONObject(1).get("type") == "Uint8Array") {
-                                val numberArr = json.getJSONObject(1).getJSONArray("data")
-                                val byteArray = ByteArray(numberArr.length())
-                                for (i in 0..<numberArr.length()) {
-                                    byteArray[i] = numberArr.get(i).toString().toInt().toByte()
-                                }
-                                data = byteArray
-                            }
-                        }catch (e: Exception) {
-                            data = json.getString(1).toByteArray()
-                        }
-
-                        var recursive = false
-                        if(json.length() > 2) {
-                            val opt = json.getJSONObject(2)
-                            try {
-                                recursive = opt.getBoolean("recursive")
-                            } catch (_: Exception) { }
-                        }
-
-                        return done(writeFile(json.getString(0), data, recursive))
-                    }
-                    "writeFileMulti" -> {
-                        var recursive = false
-                        if(json.length() > 2) {
-                            val opt = json.getJSONObject(2)
-                            try {
-                                recursive = opt.getBoolean("recursive")
-                            } catch (_: Exception) { }
-                        }
-
-
-                        val files = json.getJSONArray(0)
-                        for (i in 0..files.length()) {
-                            val file = files.getJSONObject(i)
-                            var data: ByteArray? = null
-
-                            try {
-                                val uint8array = file.getJSONObject("data")
-                                if(uint8array.get("type") == "Uint8Array") {
-                                    val numberArr = uint8array.getJSONArray("data")
-                                    val byteArray = ByteArray(numberArr.length())
-                                    for (i in 0..<numberArr.length()) {
-                                        byteArray[i] = numberArr.get(i).toString().toInt().toByte()
-                                    }
-                                    data = byteArray
-                                }
-                            }catch (e: Exception) {
-                                data = file.getString("data").toByteArray()
-                            }
-
-                            val maybeError = writeFile(file.getString("path"), data, recursive)
-                            if(maybeError != null)
-                                return done(maybeError)
-                        }
-
-                        return done(true)
-                    }
-                    "unlink" -> done(this.fs.unlink(json.getString(0)))
-                    "readdir" -> {
-                        var recursive = false
-                        var withFileTypes = false
-                        if(json.length() > 1) {
-                            val opt = json.getJSONObject(1)
-                            try {
-                                recursive = opt.getBoolean("recursive")
-                                withFileTypes = opt.getBoolean("withFileTypes")
-                            } catch (_: Exception) { }
-                        }
-
-                        return done(this.fs.readdir(json.getString(0), withFileTypes, recursive))
-                    }
-                    "mkdir" -> done(this.fs.mkdir(json.getString(0)))
-                    "rmdir" -> done(this.fs.rmdir(json.getString(0)))
-                    "stat" -> done(this.fs.stat(json.getString(0)))
-                    "lstat" -> done(this.fs.stat(json.getString(0)))
-                    "exists" -> done(this.fs.exists(json.getString(0)) ?: false)
-                }
-            }
+            "platform" -> return this.platform
+            "fs" -> return this.fsSwitch(methodPath[1], json)
+            "fetch" -> return this.fetch(json)
         }
 
         return null
     }
+
+
+    private fun fsSwitch(method: String, json: JSONArray) : Any? {
+        when (method) {
+            "readFile" -> {
+                var utf8 = false
+                if(json.length() > 1) {
+                    val opt = json.getJSONObject(1)
+                    try {
+                        utf8 = opt.getString("encoding") == "utf8"
+                    } catch (_: Exception) { }
+                }
+                return this.fs.readFile(json.getString(0), utf8)
+            }
+            "writeFile" -> {
+                var data: ByteArray? = null
+
+                try {
+                    if(json.getJSONObject(1).get("type") == "Uint8Array") {
+                        val numberArr = json.getJSONObject(1).getJSONArray("data")
+                        val byteArray = ByteArray(numberArr.length())
+                        for (i in 0..<numberArr.length()) {
+                            byteArray[i] = numberArr.get(i).toString().toInt().toByte()
+                        }
+                        data = byteArray
+                    }
+                }catch (e: Exception) {
+                    data = json.getString(1).toByteArray()
+                }
+
+                var recursive = false
+                if(json.length() > 2) {
+                    val opt = json.getJSONObject(2)
+                    try {
+                        recursive = opt.getBoolean("recursive")
+                    } catch (_: Exception) { }
+                }
+
+                return this.writeFile(json.getString(0), data, recursive)
+            }
+            "writeFileMulti" -> {
+                var recursive = false
+                if(json.length() > 2) {
+                    val opt = json.getJSONObject(2)
+                    try {
+                        recursive = opt.getBoolean("recursive")
+                    } catch (_: Exception) { }
+                }
+
+
+                val files = json.getJSONArray(0)
+                for (i in 0..files.length()) {
+                    val file = files.getJSONObject(i)
+                    var data: ByteArray? = null
+
+                    try {
+                        val uint8array = file.getJSONObject("data")
+                        if(uint8array.get("type") == "Uint8Array") {
+                            val numberArr = uint8array.getJSONArray("data")
+                            val byteArray = ByteArray(numberArr.length())
+                            for (i in 0..<numberArr.length()) {
+                                byteArray[i] = numberArr.get(i).toString().toInt().toByte()
+                            }
+                            data = byteArray
+                        }
+                    }catch (e: Exception) {
+                        data = file.getString("data").toByteArray()
+                    }
+
+                    val maybeError = this.writeFile(file.getString("path"), data, recursive)
+                    if(maybeError != null)
+                        return maybeError
+                }
+
+                return true
+            }
+            "unlink" -> return this.fs.unlink(json.getString(0))
+            "readdir" -> {
+                var recursive = false
+                var withFileTypes = false
+                if(json.length() > 1) {
+                    val opt = json.getJSONObject(1)
+                    try {
+                        recursive = opt.getBoolean("recursive")
+                    } catch (_: Exception) { }
+                    try {
+                        withFileTypes = opt.getBoolean("withFileTypes")
+                    } catch (_: Exception) { }
+                }
+
+                return this.fs.readdir(json.getString(0), withFileTypes, recursive)
+            }
+            "mkdir" -> return this.fs.mkdir(json.getString(0))
+            "rmdir" -> return this.fs.rmdir(json.getString(0))
+            "stat" -> return this.fs.stat(json.getString(0))
+            "lstat" -> return this.fs.stat(json.getString(0))
+            "exists" -> return this.fs.exists(json.getString(0)) ?: false
+        }
+
+        return null
+    }
+
+
+    private fun fetch(json: JSONArray): Any {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(json.getString(0))
+
+        var utf8 = false
+        var method = "GET"
+        val headers = mutableMapOf<String, String>()
+        var body: ByteArray? = null
+
+        if(json.length() > 1) {
+            val opt = json.getJSONObject(1)
+
+            try {
+                utf8 = opt.getString("encoding") == "utf8"
+            } catch (_: Exception) { }
+
+            try {
+                method = opt.getString("method")
+            } catch (_: Exception) { }
+
+            try {
+                val jsonHeaders = opt.getJSONObject("headers")
+                for(key in jsonHeaders.keys()) {
+                    headers[key] = jsonHeaders.getString(key)
+                }
+            } catch (_: Exception) { }
+
+            try {
+                val jsonBody = opt.getJSONObject("body")
+                if(jsonBody.getString("type") == "Uint8Array") {
+                    val numberArr = jsonBody.getJSONArray("data")
+                    val byteArray = ByteArray(numberArr.length())
+                    for (i in 0..<numberArr.length()) {
+                        val numStr = numberArr.get(i).toString()
+                        val num = numStr.toInt()
+                        byteArray[i] = num.toByte()
+                    }
+                    body = byteArray
+                }
+            } catch (_: Exception) { }
+
+            if(body == null) {
+                try {
+                    val jsonBody = opt.getString("body")
+                    body = jsonBody.toByteArray()
+                } catch (_: Exception) { }
+            }
+        }
+
+        request.method(method, body?.toRequestBody())
+
+        headers.forEach { (key, value) ->
+            request.addHeader(key, value)
+        }
+
+        if(body != null) {
+            request.addHeader("content-length", body.size.toString())
+        }
+
+        val response = client.newCall(request.build()).execute()
+
+
+        val responseJson = JSONObject()
+
+        responseJson.put("statusCode", response.code)
+        responseJson.put("statusMessage", response.message)
+
+        val responseHeaders = JSONObject()
+        response.headers.forEach { (key, value) ->
+            responseHeaders.put(key, value)
+        }
+        responseJson.put("headers", responseHeaders)
+
+        if(utf8) {
+            if(response.body == null) {
+                responseJson.put("body", "")
+            } else {
+                responseJson.put("body", response.body.toString())
+            }
+        } else {
+            val responseBody = JSONObject()
+            responseBody.put("type", "Uint8Array")
+
+            val responseBodyData = JSONArray()
+            if(response.body != null) {
+                response.body?.bytes()?.forEach { byte ->
+                    responseBodyData.put(byte)
+                }
+            }
+            responseBody.put("data", responseBodyData)
+
+            responseJson.put("body", responseBody)
+        }
+
+        return responseJson
+    }
 }
 
-data class AdapterError(val code: String, val path: String, val syscall: String)
+data class AdapterError(
+    val code: String,
+    val path: String,
+    val syscall: String,
+)
 
 class AdapterFS(private val baseDirectory: String) {
     // @return null if doesn't exist, true if exists and directory, false if exists and is file
@@ -175,14 +298,16 @@ class AdapterFS(private val baseDirectory: String) {
         return true
     }
 
-    fun unlink(path: String) {
+    fun unlink(path: String): Boolean? {
         val itemPath = this.baseDirectory + "/" + path
 
         val existsAndIsDirectory = this.itemExistsAndIsDirectory(itemPath)
-        if(existsAndIsDirectory == null || existsAndIsDirectory) return
+        if(existsAndIsDirectory == null || existsAndIsDirectory) return null
 
         val file = File(itemPath)
         file.delete()
+
+        return true
     }
 
     fun readdir(path: String, withFileTypes: Boolean, recursive: Boolean): Any {
@@ -215,18 +340,20 @@ class AdapterFS(private val baseDirectory: String) {
         return filesWithTypes
     }
 
-    fun mkdir(path: String) {
+    fun mkdir(path: String): Boolean {
         val itemPath = this.baseDirectory + "/" + path
         val dir = File(itemPath)
         dir.mkdirs()
+        return true
     }
 
-    fun rmdir(path: String) {
+    fun rmdir(path: String): Boolean? {
         val itemPath = this.baseDirectory + "/" + path
         val existsAndIsDirectory = this.itemExistsAndIsDirectory(itemPath)
-        if(existsAndIsDirectory == null || !existsAndIsDirectory) return
+        if(existsAndIsDirectory == null || !existsAndIsDirectory) return null
         val dir = File(itemPath)
         dir.deleteRecursively()
+        return true
     }
 
     fun stat(path: String) : Any? {
@@ -235,7 +362,7 @@ class AdapterFS(private val baseDirectory: String) {
         val existsAndIsDirectory = this.itemExistsAndIsDirectory(itemPath)
 
         if(existsAndIsDirectory == null) {
-            return  AdapterError(
+            return AdapterError(
                 code = "ENOENT",
                 path = path,
                 syscall = "stat"
--- platform/android/app/src/main/java/org/fullstacked/editor/Instance.kt
+++ platform/android/app/src/main/java/org/fullstacked/editor/Instance.kt
@@ -1,7 +1,243 @@
 package org.fullstacked.editor
 
-open class Instance {
-    private val adapter = Adapter()
+import android.annotation.SuppressLint
+import android.app.ActionBar.LayoutParams
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.webkit.JavascriptInterface
+import android.webkit.MimeTypeMap
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.InputStream
 
+@SuppressLint("SetJavaScriptEnabled")
+fun createWebView(
+    ctx: ComponentActivity,
+    adapter: Adapter,
+) : WebView {
+    val webView = WebView(ctx);
+    webView.setBackgroundColor(Color.TRANSPARENT)
+    val webViewClient = WebViewClientCustom(adapter)
+    webView.webViewClient = webViewClient
+    webView.settings.javaScriptEnabled = true
+    webView.loadUrl("http://localhost")
+    webView.addJavascriptInterface(webViewClient, "Android")
+    return webView
+}
 
+data class Project(val location: String, val id: String, val title: String)
+
+open class Instance(val project: Project, val context: ComponentActivity, val init: Boolean = true) {
+    lateinit var adapter: Adapter
+    lateinit var webView: WebView
+
+    init {
+        if(init) {
+            this.adapter = Adapter(
+                projectId = this.project.id,
+                baseDirectory = this.project.location
+            )
+
+            this.webView = createWebView(
+                ctx = this.context,
+                adapter = this.adapter
+            )
+
+            val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+            this.context.addContentView(this.webView, params)
+        }
+    }
+}
+
+class WebViewClientCustom(
+    private val adapter: Adapter
+) : WebViewClient() {
+    private val reqBody = HashMap<Int, String>()
+
+    @JavascriptInterface
+    fun passFetchBody(reqId: Int, body: String){
+        this.reqBody[reqId] = body
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+        view?.evaluateJavascript("const originalFetch = window.fetch; let reqId = 0; window.fetch = (...args) => { if(args?.[1]) { const id = reqId++; args[1].headers = {...(args?.[1]?.headers || {}), \"request-id\": id};  Android.passFetchBody(id, args?.[1]?.body); } return originalFetch(...args) };", null)
+    }
+
+    override fun shouldInterceptRequest(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): WebResourceResponse? {
+        if(request?.url?.host != "localhost") return super.shouldInterceptRequest(view, request);
+
+        val path = request.url?.path.toString()
+
+        var pathname = path.split("?").first()
+        if(pathname.endsWith("/")){
+            pathname = pathname.slice(0..<pathname.length - 1)
+        }
+        if(pathname.startsWith("/")) {
+            pathname = pathname.slice(1..<pathname.length)
+        }
+
+        // try for index.html
+        val maybeIndexHTML = (if(pathname.isEmpty()) "" else "$pathname/") + "index.html"
+        var inputStream: InputStream? = this.adapter.getFile(maybeIndexHTML)
+        if(inputStream != null){
+            pathname = maybeIndexHTML
+        }
+
+        // try for built file
+        if(
+            pathname.endsWith(".js") ||
+            pathname.endsWith(".css") ||
+            pathname.endsWith(".map")
+        ) {
+            val maybeBuiltFile = ".build/$pathname"
+            inputStream = this.adapter.getFile(maybeBuiltFile)
+        }
+
+        // try for the actual pathname
+        if(inputStream == null) {
+            inputStream = this.adapter.getFile(pathname)
+        }
+
+        // if we managed to get a file, respond
+        if(inputStream != null) {
+            val ext = MimeTypeMap.getFileExtensionFromUrl(pathname)
+            return WebResourceResponse(
+                MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext),
+                "",
+                inputStream
+            )
+        }
+
+        println(pathname)
+
+        // we jump into the adapter methods
+        var body: String? = null;
+        val reqIdStr = request.requestHeaders["request-id"]
+        if(reqIdStr != null) {
+            val reqId = reqIdStr.toInt()
+            if(this.reqBody[reqId] != null) {
+                body = this.reqBody[reqId]
+                this.reqBody.remove(reqId)
+            }
+        }
+
+        val methodPath = ArrayList(pathname.split("/"))
+
+        val response = when (val maybeResponseData = this.adapter.callAdapterMethod(methodPath, body)) {
+            is InputStream -> {
+                WebResourceResponse(
+                    "application/octet-stream",
+                    "binary",
+                    maybeResponseData
+                )
+            }
+
+            is String -> {
+                WebResourceResponse(
+                    "text/plain",
+                    "utf-8",
+                    200,
+                    "success",
+                    mapOf("content-length" to maybeResponseData.toByteArray().size.toString()),
+                    maybeResponseData.byteInputStream()
+                )
+            }
+
+            is AdapterError -> {
+                val json = JSONObject()
+                json.put("code", maybeResponseData.code)
+                json.put("path", maybeResponseData.path)
+                json.put("syscall", maybeResponseData.syscall)
+                val jsonStr = json.toString()
+                WebResourceResponse(
+                    "application/json",
+                    "utf-8",
+                    299,
+                    "error",
+                    mapOf("content-length" to jsonStr.toByteArray().size.toString()),
+                    jsonStr.byteInputStream()
+                )
+            }
+
+            is ByteArray -> {
+                WebResourceResponse(
+                    "application/octet-stream",
+                    "binary",
+                    200,
+                    "success",
+                    mapOf("content-length" to maybeResponseData.size.toString()),
+                    maybeResponseData.inputStream()
+                )
+            }
+
+            is Boolean -> {
+                WebResourceResponse(
+                    "application/json",
+                    "utf-8",
+                    200,
+                    "success",
+                    mapOf("content-length" to "1"),
+                    (if (maybeResponseData) "1" else "0").byteInputStream()
+                )
+            }
+
+            is List<*> -> {
+                val jsonStr = JSONArray(maybeResponseData).toString()
+                WebResourceResponse(
+                    "application/json",
+                    "utf-8",
+                    200,
+                    "success",
+                    mapOf("content-length" to jsonStr.toByteArray().size.toString()),
+                    jsonStr.byteInputStream()
+                )
+            }
+
+            is Map<*, *> -> {
+                val jsonStr = JSONObject(maybeResponseData).toString()
+                WebResourceResponse(
+                    "application/json",
+                    "utf-8",
+                    200,
+                    "success",
+                    mapOf("content-length" to jsonStr.toByteArray().size.toString()),
+                    jsonStr.byteInputStream()
+                )
+            }
+
+            is JSONObject -> {
+                val jsonStr = maybeResponseData.toString()
+                WebResourceResponse(
+                    "application/json",
+                    "utf-8",
+                    200,
+                    "success",
+                    mapOf("content-length" to jsonStr.toByteArray().size.toString()),
+                    jsonStr.byteInputStream()
+                )
+            }
+
+            else -> {
+                WebResourceResponse(
+                    "text/plain",
+                    "utf-8",
+                    404,
+                    "not found",
+                    mapOf(),
+                    "Not Found".byteInputStream()
+                )
+            }
+        }
+
+        return response
+    }
 }
