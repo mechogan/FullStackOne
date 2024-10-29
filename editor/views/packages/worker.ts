7c751dbdba76fd6dfcbbd77084c71ccee4fec00e
cplepage <lepage.charlespilippe@gmail.com>
2024-10-29 17:00:51 -0400

    WIP Files

---
+++ editor/views/packages/worker.ts
@@ -0,0 +1,158 @@
+import "./worker-env";
+import rpc from "../../rpc";
+import gzip from "gzip-js";
+import untar from "js-untar";
+
+export type PackageInstallerWorkerMessage = {
+    type: "ready"
+} | {
+    name: string,
+    type: "progress",
+    status: string,
+    loaded?: number,
+    total?: number
+} | {
+    name: string,
+    type: "dependencies",
+    packages: string[]
+} | {
+    name: string,
+    type: "done",
+    success: boolean
+}
+
+const nodeModulesDirectory = await rpc().directories.nodeModulesDirectory();
+const td = new TextDecoder();
+
+const maxPayloadSize = 100000; // 100kb
+const maxFilesPerPaylod = 10;
+
+let installing = false;
+
+self.onmessage = (message: MessageEvent) => {
+    console.log("Worker", message.data)
+    install(message.data)
+        .then(() => sendMessage({ 
+            name: message.data,
+            type: "done",
+            success: true
+         }))
+         .catch(() => sendMessage({ 
+            name: message.data,
+            type: "done",
+            success: false
+         }));
+}
+
+const sendMessage = (message: PackageInstallerWorkerMessage) => {
+    self.postMessage(message);
+}
+
+async function install(name: string){
+    if(installing) {
+        console.error("Trying to install a package on a worker already in an installation process.");
+        return;
+    };
+
+    sendMessage({
+        name,
+        type: "progress",
+        status: "downloading"
+    });
+
+    const packageInfoStr = (
+        await rpc().fetch(`https://registry.npmjs.org/${name}/latest`, {
+            encoding: "utf8"
+        })
+    ).body as string;
+    const packageInfoJSON = JSON.parse(packageInfoStr);
+    const tarbalUrl = packageInfoJSON.dist.tarball;
+    const tarballData = (await rpc().fetch(tarbalUrl)).body as Uint8Array;
+
+    sendMessage({
+        name,
+        type: "progress",
+        status: "unpacking"
+    });
+    const tarData = new Uint8Array(gzip.unzip(tarballData));
+    await rpc().fs.mkdir(`${nodeModulesDirectory}/${name}`, {
+        absolutePath: true
+    });
+    const files: {
+        name: string;
+        buffer: ArrayBufferLike;
+        type: string; // https://en.wikipedia.org/wiki/Tar_(computing)#UStar_format
+    }[] = await untar(tarData.buffer);
+
+    let filesToWrite: { path: string; data: Uint8Array }[] = [];
+    const writeFiles = async () => {
+        await rpc().fs.writeFileMulti(filesToWrite, {
+            absolutePath: true,
+            recursive: true
+        });
+        filesToWrite = [];
+    };
+
+    const packageJSONFile = `${nodeModulesDirectory}/${name}/package.json`;
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.type === "5") continue;
+
+        const pathComponents = file.name.split("/").slice(1); // strip 1
+        const path = `${nodeModulesDirectory}/${name}/${pathComponents.join("/")}`;
+
+        if (path === packageJSONFile) {
+            const packageJSON = JSON.parse(td.decode(file.buffer));
+            if (packageJSON.dependencies) {
+                sendMessage({
+                    name,
+                    type: "dependencies",
+                    packages: Object.keys(packageJSON.dependencies)
+                })
+            }
+        }
+
+        let currentPayloadSize = filesToWrite.reduce(
+            (sum, { data }) => sum + data.byteLength,
+            0
+        );
+
+        if (
+            currentPayloadSize >= maxPayloadSize ||
+            filesToWrite.length >= maxFilesPerPaylod
+        ) {
+            await writeFiles();
+        }
+
+        filesToWrite.push({
+            path,
+            data: new Uint8Array(file.buffer)
+        });
+
+        sendMessage({
+            name,
+            type: "progress",
+            status: `(${i}/${files.length}) unpacking`,
+            loaded: i + 1,
+            total: files.length
+        });
+    }
+
+    // maybe leftovers
+    if (filesToWrite.length) {
+        await writeFiles();
+    }
+
+    sendMessage({
+        name,
+        type: "progress",
+        status: "installed",
+        loaded: 1,
+        total: 1
+    });
+}
+
+sendMessage({
+    type: "ready"
+})
