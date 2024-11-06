92b5bb2628721b56e731125c11690e7fda2c034c
cplepage <lepage.charlespilippe@gmail.com>
2024-11-06 15:40:14 -0500

    window vs setup

---
+++ platform/windows/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace windows
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            this.editor = new WebView();
+        }
+
+        private WebView editor;
+    }
+}
---
+++ platform/windows/WebView.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using Windows.UI.WebUI;
+
+namespace windows
+{
+    internal class WebView
+    {
+        private Window window;
+        private WebView2 webview;
+
+        public WebView() {
+            this.window = new Window();
+            this.webview = new WebView2();
+            window.Content = this.webview;
+            window.Activate();
+            this.Init();
+        }
+
+        async public void Init() {
+            await this.webview.EnsureCoreWebView2Async();
+            this.webview.Source = new Uri("https://fullstacked.org");
+        }
+    }
+}
