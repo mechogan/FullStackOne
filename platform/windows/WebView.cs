c87452c594371f981a264d109293b96ebbf8f682
cplepage <lepage.charlespilippe@gmail.com>
2024-11-06 16:00:09 -0500

    intercepting request in webview2

--- platform/windows/WebView.cs
+++ platform/windows/WebView.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualBasic;
+using Microsoft.Web.WebView2.Core;
 using System;
-using Windows.UI.WebUI;
+using System.IO;
+using System.Text;
+using Windows.Storage.Streams;
 
 namespace windows
 {
@@ -10,7 +14,8 @@ namespace windows
         private Window window;
         private WebView2 webview;
 
-        public WebView() {
+        public WebView()
+        {
             this.window = new Window();
             this.webview = new WebView2();
             window.Content = this.webview;
@@ -18,9 +23,17 @@ namespace windows
             this.Init();
         }
 
-        async public void Init() {
+        async public void Init()
+        {
             await this.webview.EnsureCoreWebView2Async();
-            this.webview.Source = new Uri("https://fullstacked.org");
+            this.webview.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
+            this.webview.CoreWebView2.WebResourceRequested += delegate (CoreWebView2 sender, CoreWebView2WebResourceRequestedEventArgs args)
+            {
+                IRandomAccessStream stream = new MemoryStream(Encoding.UTF8.GetBytes("<h1>FullStacked</h1>")).AsRandomAccessStream();
+                CoreWebView2WebResourceResponse response = this.webview.CoreWebView2.Environment.CreateWebResourceResponse(stream, 200, "OK", "Content-Type: text/html");
+                args.Response = response;
+            };
+            this.webview.Source = new Uri("http://localhost");
         }
     }
 }
