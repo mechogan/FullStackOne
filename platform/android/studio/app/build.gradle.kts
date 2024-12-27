9b1425843968db20fbcf7b3d7486f69a5b47b57a
cplepage <lepage.charlesphilippe@gmail.com>
2024-12-27 09:57:54 -0500

    released

--- platform/android/studio/app/build.gradle.kts
+++ platform/android/studio/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "org.fullstacked.editor"
         minSdk = 29
         targetSdk = 34
-        versionCode = 744
+        versionCode = 767
         versionName = "0.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
