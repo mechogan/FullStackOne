9f32823164853c885d25b8c7e99ed664f5a3afff
cplepage <lepage.charlesphilippe@gmail.com>
2025-02-21 13:11:29 -0500

    removed windows debug

--- editor/index.ts
+++ editor/index.ts
@@ -11,12 +11,12 @@ import platform, { Platform } from "../lib/platform";
 core_message.addListener("deeplink", deeplink);
 
 // fix windows scrollbars
-// if (navigator.userAgent.includes("Windows")) {
+if (navigator.userAgent.includes("Windows")) {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = "/windows.css";
     document.head.append(link);
-// }
+}
 
 document.querySelector("#splash")?.remove();
 Projects();
@@ -28,7 +28,7 @@ core_message.addListener("package", (dataStr) => {
         console.log(dataStr);
     }
 });
-WindowsAskForAdmin();
+
 const checkProjectsConfigExists = await config.get(CONFIG_TYPE.PROJECTS, true);
 if (!checkProjectsConfigExists) {
     if (platform === Platform.WINDOWS) {
