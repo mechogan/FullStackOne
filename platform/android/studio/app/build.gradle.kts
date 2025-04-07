39592c5759ee5234ba9a73b373960b13045078e9
cplepage <lepage.charlesphilippe@gmail.com>
2025-04-07 10:32:36 -0400

    streaming-markdown update

--- platform/android/studio/app/build.gradle.kts
+++ platform/android/studio/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "org.fullstacked.editor"
         minSdk = 29
         targetSdk = 34
-        versionCode = 966
+        versionCode = 968
         versionName = "0.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
