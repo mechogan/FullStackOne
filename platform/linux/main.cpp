84032dd46a82b0ffa4df32bba6a7c8148b0c1d23
cplepage <lepage.charlesphilippe@gmail.com>
2025-04-08 05:24:58 +0000

    almost there

--- platform/linux/main.cpp
+++ platform/linux/main.cpp
@@ -5,11 +5,13 @@
 #include <iostream>
 #include "./bin/linux-x86_64.h"
 
-std::string getexepath()
+std::string getexedir()
 {
     char result[PATH_MAX];
     ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
-    return std::string(result, (count > 0) ? count : 0);
+    std::string path = std::string(result, (count > 0) ? count : 0);
+    std::size_t pos = path.find_last_of("/");
+    return path.substr(0, pos);
 }
 
 void setDirectories()
@@ -17,7 +19,7 @@ void setDirectories()
     std::string home = getenv("HOME");
     std::string root = home + "/FullStacked";
     std::string config = home + "/.config/fullstacked";
-    std::string editor = getexepath() + "/editor";
+    std::string editor = getexedir() + "/editor";
 
     directories(
         root.data(),
@@ -47,7 +49,7 @@ int bytesToNumber(char *bytes, int size)
         value = value << 8;
         value = value | bytes[i];
     }
-    return (int)value;
+    return value;
 }
 
 int deserializeNumber(char* bytes, int size)
@@ -72,20 +74,27 @@ int deserializeNumber(char* bytes, int size)
     return value;
 }
 
+
+void printBuffer(char *buffer, int size) {
+    for(int i = 0; i < size; i++) {
+        std::cout << (int)buffer[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 int combineBuffers(char *buf1, int lgt1, char *buf2, int lgt2, char *result)
 {
     int combinedLength = lgt1 + lgt2;
-    result = new char[combinedLength];
+    char* combined = new char[combinedLength];
     for (int i = 0; i < lgt1; i++)
     {
-        result[i] = buf1[i];
+        combined[i] = buf1[i];
     }
     for (int i = 0; i < lgt2; i++)
     {
-        result[i + lgt1] = buf2[i];
+        combined[i + lgt1] = buf2[i];
     }
-    delete buf1;
-    delete buf2;
+    memcpy(result, combined, combinedLength);
     return combinedLength;
 }
 
@@ -116,13 +125,12 @@ std::vector<DataValue*> deserializeArgs(char *data, int size)
     {
         DataType type = (DataType)data[cursor];
 
-        char *lengthData;
-        char *arg;
+        char *lengthData = new char[0];
+        char *arg = new char[0];
 
         cursor++;
         memcpy(lengthData, data + cursor, 4);
         int length = bytesToNumber(lengthData, 4);
-        delete lengthData;
 
         cursor += 4;
         memcpy(arg, data + cursor, length);
@@ -140,9 +148,10 @@ std::vector<DataValue*> deserializeArgs(char *data, int size)
             v->number = deserializeNumber(arg, length);
             break;
         case STRING:
-            v->str = std::string(arg);
+            v->str = std::string(arg, length);
             break;
         case BUFFER:
+            v->buffer = new char[0];
             memcpy(v->buffer, arg, length);
             v->bufferSize = length;
             break;
@@ -150,7 +159,6 @@ std::vector<DataValue*> deserializeArgs(char *data, int size)
             break;
         }
         args.push_back(v);
-        delete arg;
     }
 
     return args;
@@ -163,7 +171,7 @@ class Instance : public Gtk::Window
 private:
     std::string id;
     bool isEditor;
-    char *header;
+    char *header = new char[0];
     int headerSize;
 
 public:
@@ -173,7 +181,7 @@ public:
 
         std::string pathname = webkit_uri_scheme_request_get_path(request);
 
-        std::cout << pathname << std::endl;
+        std::cout << "PATH: " << pathname << std::endl;
 
         char *responseData;
         int responseSize;
@@ -190,22 +198,32 @@ public:
             payloadBodyHeader[0] = 1; // Static File Serving
             payloadBodyHeader[1] = 2; // STRING
 
-            char *pathnameData = pathname.data();
-            int pathnameSize = strlen(pathnameData);
+            char *pathnameData = pathname.empty() ? new char[0] : pathname.data();
+            int pathnameSize = pathname.size();
             char *pathnameSizeBuffer = numberToByte(pathnameSize);
 
-            char *payloadBody;
-            int payloadBodySize = combineBuffers(
+            char *payloadBodyBody = new char[0];
+            int payloadBodyBodySize = combineBuffers(
                 pathnameSizeBuffer,
                 4,
                 pathnameData,
                 pathnameSize,
-                payloadBody);
+                payloadBodyBody);
 
-            char *tmpHeader;
+
+            char *payloadBody = new char[0];
+            int payloadBodySize = combineBuffers(
+                payloadBodyHeader,
+                2,
+                payloadBodyBody,
+                payloadBodyBodySize,
+                payloadBody
+            );
+
+            char *tmpHeader = new char[0];
             memcpy(tmpHeader, instance->header, instance->headerSize);
 
-            char *payload;
+            char *payload = new char[0];
             int payloadSize = combineBuffers(
                 tmpHeader,
                 instance->headerSize,
@@ -213,10 +231,10 @@ public:
                 payloadBodySize,
                 payload);
 
-            void *responseData;
-            int responseSize = call(payload, payloadSize, &responseData);
+            void *libResponseData = new char[0];
+            int libResponseSize = call(payload, payloadSize, &libResponseData);
 
-            std::vector<DataValue*> values = deserializeArgs((char *)responseData, responseSize);
+            std::vector<DataValue*> values = deserializeArgs((char *)libResponseData, libResponseSize);
 
             responseType = values.at(0)->str;
             responseData = values.at(1)->buffer;
@@ -251,7 +269,7 @@ public:
 
         GInputStream *inputStream = g_memory_input_stream_new();
         g_memory_input_stream_add_data(G_MEMORY_INPUT_STREAM(inputStream), responseData, responseSize, nullptr);
-        webkit_uri_scheme_request_finish(request, inputStream, responseSize, responseType.data());
+        webkit_uri_scheme_request_finish(request, inputStream, responseSize, responseType.c_str());
     }
 
     Instance(std::string pId, bool pIsEditor)
@@ -276,7 +294,7 @@ public:
             isEditorHeader[0] = 0;
 
             char *idStrData = id.data();
-            int idStrLength = strlen(idStrData);
+            int idStrLength = id.size();
 
             char *intermediateBuffer;
             headerSize = combineBuffers(
