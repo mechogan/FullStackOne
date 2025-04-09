84fed2a3cef41af7e3ae5d7de74116200b8d4404
cplepage <lepage.charlesphilippe@gmail.com>
2025-04-09 03:35:53 +0000

    organizing

--- platform/linux/build.sh
+++ platform/linux/build.sh
@@ -2,4 +2,4 @@ rm -rf out
 mkdir out
 cp -r ../../core/bin .
 cp -r ../../out/editor ./out
-g++ main.cpp bin/linux-x86_64 -g -o out/fullstacked `pkg-config gtkmm-4.0 webkitgtk-6.0 --libs --cflags`
+g++ utils.cpp instance.cpp app.cpp main.cpp bin/linux-x86_64 -g -o out/fullstacked `pkg-config gtkmm-4.0 webkitgtk-6.0 --libs --cflags`
