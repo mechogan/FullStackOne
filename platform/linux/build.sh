450b22c109d85acf2727687e0b07dfb879d345a6
cplepage <lepage.charlesphilippe@gmail.com>
2025-04-10 12:22:11 -0400

    wip

--- platform/linux/build.sh
+++ platform/linux/build.sh
@@ -1,5 +1,8 @@
 rm -rf out
-mkdir out
+mkdir -p out/usr/bin
+cp -r ../../out/editor ./out/usr/bin
+mkdir ./out/DEBIAN
+cp control out/DEBIAN/control
+
 cp -r ../../core/bin .
-cp -r ../../out/editor ./out
-g++ utils.cpp instance.cpp app.cpp main.cpp bin/linux-x86_64 -o out/fullstacked `pkg-config gtkmm-4.0 webkitgtk-6.0 --libs --cflags`
+g++ utils.cpp instance.cpp app.cpp main.cpp bin/linux-x86_64 -o out/usr/bin/fullstacked `pkg-config gtkmm-4.0 webkitgtk-6.0 --libs --cflags`
