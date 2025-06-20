ab6377677a39d20d594d4c9d34334abefa431e63
cplepage <lepage.charlesphilippe@gmail.com>
2025-06-20 13:45:24 -0400

    removed windows 32 bit

--- core/build/windows.bat
+++ core/build/windows.bat
@@ -11,10 +11,6 @@ go build -buildmode=c-shared -o ../bin/win32-arm64.dll -v ..
 SET GOARCH ="amd64"
 go build -buildmode=c-shared -o ../bin/win32-x64.dll -v ..
 
-SET GOARCH ="386"
-go build -buildmode=c-shared -o ../bin/win32-ia32.dll -v ..
-
-xcopy ..\bin\win32-ia32.dll ..\..\platform\windows /y /q
 xcopy ..\bin\win32-x64.dll ..\..\platform\windows /y /q
 xcopy ..\bin\win32-arm64.dll ..\..\platform\windows /y /q
 xcopy ..\..\out\editor ..\..\platform\windows\editor /y /s /e /q
