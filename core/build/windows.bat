dd1af3fa326b06a70a58e6ec1bc9f5b107c48546
cplepage <lepage.charlesphilippe@gmail.com>
2025-06-20 16:25:53 -0400

    windows msix x64 and arm64 functional

--- core/build/windows.bat
+++ core/build/windows.bat
@@ -2,13 +2,17 @@
 @REM https://github.com/mstorsjo/llvm-mingw
 
 @ECHO off
-SET CGO_ENABLED ="1"
-SET GOOS ="windows"
+SET CGO_ENABLED=1
+SET GOOS=windows
 
-SET GOARCH ="arm64"
+SET CC=aarch64-w64-mingw32-gcc
+SET CXX=aarch64-w64-mingw32-g++
+SET GOARCH=arm64
 go build -buildmode=c-shared -o ../bin/win32-arm64.dll -v ..
 
-SET GOARCH ="amd64"
+SET CC=x86_64-w64-mingw32-gcc
+SET CXX=x86_64-w64-mingw32-g++
+SET GOARCH=amd64
 go build -buildmode=c-shared -o ../bin/win32-x64.dll -v ..
 
 xcopy ..\bin\win32-x64.dll ..\..\platform\windows /y /q
