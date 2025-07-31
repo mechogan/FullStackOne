8f5d35c259d0110b9f982e7b61b20f252b0dc29a
cplepage <lepage.charlesphilippe@gmail.com>
2025-07-31 00:35:59 -0400

    fs tools

--- fullstacked_modules/ai/index.ts
+++ fullstacked_modules/ai/index.ts
@@ -1,12 +1,18 @@
 
 import * as aiAgent from "@fullstacked/ai-agent";
 import { z as zod } from "zod";
+import fs from "./tools/fs"
 
 export * from "@fullstacked/ai-agent";
 
 export const z = zod;
 
+export const tools = {
+    fs
+}
+
 export default {
     ...aiAgent,
+    tools,
     z: zod
 }
