1d4a6baa93fe50f855647228d81c367aff71e09c
cplepage <lepage.charlesphilippe@gmail.com>
2025-07-31 12:35:42 -0400

    better fs tools spawn

--- fullstacked_modules/ai/index.ts
+++ fullstacked_modules/ai/index.ts
@@ -1,17 +1,14 @@
 import * as aiAgent from "@fullstacked/ai-agent";
 import { z as zod } from "zod";
-import fs from "./tools/fs";
+export * from "./tools/fs"; 
+import { createToolFS } from "./tools/fs";
 
 export * from "@fullstacked/ai-agent";
 
 export const z = zod;
 
-export const tools = {
-    fs
-};
-
 export default {
     ...aiAgent,
-    tools,
+    createToolFS,
     z: zod
 };
--- fullstacked_modules/ai/tools/fs.ts
+++ fullstacked_modules/ai/tools/fs.ts
@@ -2,44 +2,56 @@ import { createTool } from "@fullstacked/ai-agent";
 import { z } from "zod";
 import fs from "../../fs";
 
-const tools: ReturnType<typeof createTool>[] = [
-    createTool({
-        name: "CreateDir",
-        description: "Create a directory.",
-        schema: z.object({
-            path: z.string()
-        }),
-        fn: ({ path }) => fs.mkdir(path),
-        message: ({ path }) => `Creating directory ${path}`
-    }),
-    createTool({
-        name: "ListFiles",
-        description: "List files in a directory.",
-        schema: z.object({
-            path: z.string()
-        }),
-        fn: ({ path }) => fs.readdir(path),
-        message: ({ path }) => `Listing files at ${path}`
-    }),
-    createTool({
-        name: "ReadFile",
-        description: "Read the content of a file.",
-        schema: z.object({
-            path: z.string()
-        }),
-        fn: ({ path }) => fs.readFile(path, { encoding: "utf8" }),
-        message: ({ path }) => `Reading file at ${path}`
-    }),
-    createTool({
-        name: "WriteFile",
-        description: "Write content to file.",
-        schema: z.object({
-            path: z.string(),
-            contents: z.string()
-        }),
-        fn: ({ path, contents }) => fs.writeFile(path, contents),
-        message: ({ path }) => `Writing to ${path}`
-    })
-];
+type ToolFSOptions = {
+    baseDirectory: string
+}
 
-export default tools;
+export function createToolFS(opts?: Partial<ToolFSOptions>) {
+    let basePath = opts?.baseDirectory || "";
+
+    if(basePath !== "" && !basePath.endsWith("/")) {
+        basePath += "/";
+    }
+
+    const fixPath = (path: string) => basePath + path;
+
+    return [
+        createTool({
+            name: "CreateDir",
+            description: "Create a directory.",
+            schema: z.object({
+                path: z.string()
+            }),
+            fn: ({ path }) => fs.mkdir(fixPath(path)),
+            message: ({ path }) => `Creating directory \`${path}\``
+        }),
+        createTool({
+            name: "ListFiles",
+            description: "List files in a directory.",
+            schema: z.object({
+                path: z.string()
+            }),
+            fn: ({ path }) => fs.readdir(fixPath(path)),
+            message: ({ path }) => `Listing files at \`${path}\``
+        }),
+        createTool({
+            name: "ReadFile",
+            description: "Read the content of a file.",
+            schema: z.object({
+                path: z.string()
+            }),
+            fn: ({ path }) => fs.readFile(fixPath(path), { encoding: "utf8" }),
+            message: ({ path }) => `Reading file at \`${path}\``
+        }),
+        createTool({
+            name: "WriteFile",
+            description: "Write content to file.",
+            schema: z.object({
+                path: z.string(),
+                contents: z.string()
+            }),
+            fn: ({ path, contents }) => fs.writeFile(fixPath(path), contents),
+            message: ({ path }) => `Writing to \`${path}\``
+        })
+    ];
+}
